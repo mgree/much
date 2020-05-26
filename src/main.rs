908b8d3e952f40f967e6f61353253301b2f9ba48
Michael Greenberg <michael.greenberg@pomona.edu>
2020-05-26 10:26:02 -0700

    version info

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io;
 
 use tokio::net::TcpListener;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let addr = env::args()
@@ -11,5 +13,9 @@ async fn main() -> io::Result<()> {
     
     let mut listener = TcpListener::bind(&addr).await?;
 
+    println!("much v{} starting up on {}", VERSION, addr);
+
+
+    println!("much v{} shutting down on {}", VERSION, addr);
     Ok(())
 }
