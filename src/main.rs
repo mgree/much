d09c6d6c1b027474bd2349553bb1fd851d54654a
Michael Greenberg <michael.greenberg@pomona.edu>
2020-05-26 10:22:51 -0700

    add in tokio features, binding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io;
+
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:4000".to_string());
+    
+    let mut listener = TcpListener::bind(&addr).await?;
+
+    Ok(())
 }
